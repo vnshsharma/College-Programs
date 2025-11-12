487da7083fdf572a2a4596a245c8160b6a2fd149
Vansh Sharma <xplore.vansh@gmail.com>
2025-11-12 19:52:10 +0530

    Final Code of Hydrogen Atom

--- Quantum/hydrogen_atom.py
+++ Quantum/hydrogen_atom.py
@@ -1,7 +1,7 @@
-import numpy as np
+import numpy as np 
 import matplotlib.pyplot as plt 
-N = 400
-x = np.linspace(1e-7,4,N)
+N = 800
+x = np.linspace(1e-7,30,N)
 h = x[1]-x[0]
 e = 3.795
 hcut = 1973
@@ -16,15 +16,18 @@ for i in range(N):
     if i<N-1:
         H[i,i+1] = -coeff
 E,psi = np.linalg.eigh(H)
-psi /= np.sqrt(np.trapezoid(psi**2,x))
+for i in range(N):
+    psi[:,i] /= np.sqrt(np.trapezoid(psi[:,i]**2,x))
 for idx,n in enumerate([1,2]):
     plt.subplot(1,2,idx+1)
-    plt.plot(x,psi[:,n]**2,label=f'Energy={E[n]:.2f}')
+    plt.plot(x,psi[:,n]**2,label=f'E={E[n]:.2f}')
     plt.title(f'n={n}')
     plt.xlabel('x')
+    plt.xlim(-1,10)
     plt.ylabel('Energy')
     plt.legend()
     plt.grid()
-plt.suptitle('Hydrogen Atom Model')
+    print(f'for n={n}, Energy={E[n]:.2f}')
+plt.suptitle('Hydrogen Atom')
 plt.tight_layout()
-plt.show() 
+plt.show()
