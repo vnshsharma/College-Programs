c505edee016738798de79e8d26c1cc10fbc813cb
Vansh Sharma <xplore.vansh@gmail.com>
2025-12-13 10:33:56 +0530

    2D-Vectors

---
+++ Data Structures and Algo/2D-Vectors/code.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<vector<int>> matrix = {{1,2,3},{4,5,6},{7,8,9}};
+    
+    // rows=> matrix.size();
+    // cols=> matrix[i].size();
+
+    for (int i=0; i<matrix.size(); i++){
+        for (int j=0; j<matrix[i].size(); j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
